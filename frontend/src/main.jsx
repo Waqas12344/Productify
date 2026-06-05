ee341c36ff5fd1c4d10e81895c43ff51004e6894
Waqas12344 <wm635950@gmail.com>
2026-06-05 16:16:40 +0500

    feat: initialize the project , install daizy UI , clerk , tailwind css and setup it on the project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from '@clerk/react'
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error(
+//     "VITE_PUBLISHABLE_KEY is not defined in the environment variables",
+//   );
+// }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
